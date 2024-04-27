4e392a2319e860d946d259b4c66e403339a4d835
krish7-dev <krishnaanilkumar.dev@gmail.com>
2024-04-27 16:11:00 +0530

    first

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className='nav_web'>
+        <a className='more'>≡</a>
+        <div></div>
+        <a className='nav_items_web' id='about' href="#">About</a>
+        <a className='nav_items_web' id='projects' href="#">Projects</a>
+        <a className='nav_items_web' id='contact' href="#">Contact</a>
+        <div></div>
+      </nav>
+
+      <nav className='nav_mob'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </nav>
+
+      <div className="name">Krishna</div>
+      <div className="name">Anilkumar</div>
     </div>
   );
 }
